31301b0612d350885940d709ea138a08c373b0f4
Prashanth-DevO <prashanthsakapuram@gmail.com>
2026-03-31 22:16:47 +0530

    small bug fix

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -26,7 +26,7 @@ const registerUser = async (req, res) => {
         res.cookie("token", token,{
             httpOnly: true,
             sameSite: "none",
-            secure: false,
+            secure: true,
             maxAge: 24 * 60 * 60 * 1000
         })
         await registrationEmail(newUser);
